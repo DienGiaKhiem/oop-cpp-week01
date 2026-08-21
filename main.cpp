480c5aed36e49199b76b85f2aed0ba6c0e81f976
DienGiaKhiem <kennyjacky2410@gmail.com>
2026-08-21 08:24:18 +0700

    Add formatted output

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello, My name is Dien Gia Khiem!" << std::endl;
-    std::cout << "Welcome to OOP with C++" << std::endl;
+    std::cout << "========================\n";
+    std::cout << "    MY FIRST C++ APP    \n";
+    std::cout << "========================\n";
+    std::cout << "Name: Dien Gia Khiem\n";
+    std::cout << "Course: OOP with C++\n";
+    std::cout << "I'm ready to learn C++!\n";
     return 0;
 }
