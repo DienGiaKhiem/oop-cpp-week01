e3874f7a3d17642e71849ebb5c01ca30325d5b59
DienGiaKhiem <kennyjacky2410@gmail.com>
2026-08-21 09:16:16 +0700

    Complete Part 3 Exercises and Personal Challenge

--- main.cpp
+++ main.cpp
@@ -8,5 +8,13 @@ int main()
     std::cout << "Name: Dien Gia Khiem\n";
     std::cout << "Course: OOP with C++\n";
     std::cout << "I'm ready to learn C++!\n";
+    std::cout << "\n";
+    std::cout << "My favorite language: C++\n";
+    std::cout << "My goal: software engineer\n";
+    std::cout << "My message: Never stop learning\n";
+    std::cout <<"========================\n";
+    std::cout << "     KEEP GOING        \n";
+    std::cout <<"========================\n";
+
     return 0;
 }
